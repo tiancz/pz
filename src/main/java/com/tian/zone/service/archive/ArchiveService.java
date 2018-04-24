ae6c0b7d8e8ac24b6e690fa27cfa1d1c12e3d613
natha <natha@tiancz>
2018-04-25 00:19:15 +0800

    archive

---
+++ src/main/java/com/tian/zone/service/archive/ArchiveService.java
@@ -0,0 +1,15 @@
+package com.tian.zone.service.archive;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * <p>Title:ArchiveService</p>
+ * <p>Description:</p>
+ * @author tianchaozhe665
+ * @Email nathanieltian@163.com
+ * @date 2018年4月24日 下午11:39:45
+ **/
+public interface ArchiveService {
+	public JSONObject getYear(JSONObject req);
+	public JSONObject getMonth(JSONObject req);
+}
