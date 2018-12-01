0eb08e931334c33505d63f446bc30f5829dce1d9
chaozhe tian <nathanieltian@163.com>
2018-12-01 23:07:50 +0800

    update spring version

--- src/test/java/com/tian/zone/article/ArticleTest.java
+++ src/test/java/com/tian/zone/article/ArticleTest.java
@@ -7,5 +7,5 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="classpath:core/applicationContext.xml")
 public class ArticleTest {
-	
+
 }
